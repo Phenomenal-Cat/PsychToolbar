6b5e2b25c0997478ec2f647daf30574aca6c4b49
Phenomenal-Cat <aidanmurphy1@hotmail.co.uk>
2021-04-26 13:21:19 -0400

    Added Jenkins file

---
+++ NTB_Utils/NTB_SplashScreen.m
@@ -0,0 +1,18 @@
+function app = NTB_SplashScreen(app)
+
+
+SplashLogo = 'NIFmagnetic.png';
+[im,~, alpha] = imread(SplashLogo);
+im(:,:,4) = alpha;
+app.Run.SplashTex = Screen('MakeTexture', app.Run.Win, im);
+SourceRect = round([1,1,size(im,2),size(im,1)]/2);
+
+ScreenIDtext = {'Experimenter display', 'Subject display'};
+Screen('FillRect', app.Run.Win, [0.5,0.5,0.5]);
+Screen('TextSize', app.Run.Win, 60);
+Screen('TextFont', app.Run.Win, 'Arial');
+for d = 1:2
+    Screen('DrawText', app.Run.SplashTex, sprintf('NIF Toolbar V 1.0\n\n%s',ScreenIDtext{d}) , 10, 10, [0,0,0], app.Params.Display.Screen.BackgroundColor);
+end
+Screen('DrawTexture', app.Run.Win, app.Run.SplashTex, [], CenterRect(SourceRect, ScreenRect));
+%Screen('Flip', app.Run.Win);
