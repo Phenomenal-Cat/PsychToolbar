65fa7497b4382946be60d65dfe25fb81dc4e1704
Phenomenal-Cat <aidanmurphy1@hotmail.co.uk>
2021-04-27 13:41:39 -0400

    Added base.mk

--- NTB_Docs/make.bat
+++ NTB_Docs/make.bat
@@ -7,8 +7,8 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=NTB_Docs/source
-set BUILDDIR=NTB_Docs/build
+set SOURCEDIR=source
+set BUILDDIR=build
 
 if "%1" == "" goto help
 
